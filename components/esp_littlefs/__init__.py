d363799d18d69dfb09eec0ffa6dd794adcf08d82
Vitus-TL <emanuel76@gmail.com>
2026-02-19 13:36:37 +0100

    Modelled __init__.py from the one for i18n

--- components/esp_littlefs/__init__.py
+++ components/esp_littlefs/__init__.py
@@ -1,20 +1,32 @@
 """ESPHome external component wrapper for joltwallet/esp_littlefs."""
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import component
+from esphome.components import esp32
 from esphome.const import CONF_ID
-from esphome.cpp_types import ESPIDFComponent
+import os
 
 DEPENDENCIES = ["esp32"]
 CODEOWNERS = ["@VitusTL"]
-AUTO_LOAD = ["littlefs"]
+
+AUTO_LOAD = []
 
 littlefs_ns = cg.esphome_ns.namespace("littlefs")
-LittleFSComponent = cg.CPPClass("LittleFSComponent", cg.Component)
+LittleFSComponent = littlefs_ns.class_("LittleFSComponent", cg.Component)
 
-CONFIG_SCHEMA = component.COMPONENT_SCHEMA
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(LittleFSComponent),
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
+    # Expose the include directory so user code can #include "esp_littlefs.h"
+    component_dir = os.path.dirname(os.path.abspath(__file__))
+    include_dir = os.path.join(component_dir, "include")
+    cg.add_build_flag(f"-I{include_dir}")
+
+    cg.add_define("USE_ESP_LITTLEFS")
