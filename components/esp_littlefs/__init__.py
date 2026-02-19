4e7a2a9b1dadbb43b9f3bd9980b5eee25084b425
Vitus-TL <emanuel76@gmail.com>
2026-02-19 13:47:12 +0100

    Attempt to fix esp_littlefs.h not found when using this component

--- components/esp_littlefs/__init__.py
+++ components/esp_littlefs/__init__.py
@@ -1,13 +1,13 @@
 """ESPHome external component wrapper for joltwallet/esp_littlefs."""
+import os
+import shutil
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32
 from esphome.const import CONF_ID
-import os
+from esphome.core import CORE
 
 DEPENDENCIES = ["esp32"]
 CODEOWNERS = ["@VitusTL"]
-
 AUTO_LOAD = []
 
 littlefs_ns = cg.esphome_ns.namespace("littlefs")
@@ -24,9 +24,10 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    # Expose the include directory so user code can #include "esp_littlefs.h"
+    # Copy esp_littlefs.h into src/ so it's findable via -Isrc
     component_dir = os.path.dirname(os.path.abspath(__file__))
-    include_dir = os.path.join(component_dir, "include")
-    cg.add_build_flag(f"-I{include_dir}")
+    src_header = os.path.join(component_dir, "include", "esp_littlefs.h")
+    dest_header = CORE.relative_src_path("esp_littlefs.h")
+    shutil.copy2(src_header, dest_header)
 
     cg.add_define("USE_ESP_LITTLEFS")
